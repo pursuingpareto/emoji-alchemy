0bbaac4ffe5084b021c8d7a94a628ce4006b37ae
AndyAtUdacity <pursuingpareto@gmail.com>
2015-04-03 12:09:14 -0700

    Initial Commit

---
+++ Emoji/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoji
+//
+//  Created by Andy Brown on 4/3/15.
+//  Copyright (c) 2015 Andy Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
