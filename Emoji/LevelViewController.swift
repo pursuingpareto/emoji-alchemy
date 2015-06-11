1dc551d47398bac2d76926a63f06aa3cd78c0ccc
Andy Brown <andy@udacity.com>
2015-06-10 17:13:45 -0700

    add simple level mode

--- Emoji/LevelViewController.swift
+++ Emoji/LevelViewController.swift
@@ -1,128 +1,40 @@
 //
-//  ViewController.swift
+//  LevelViewController.swift
 //  Emoji
 //
-//  Created by Andy Brown on 4/3/15.
+//  Created by Andrew Brown on 6/10/15.
 //  Copyright (c) 2015 Andy Brown. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController, UIGestureRecognizerDelegate {
-    
+class LevelViewController: UIViewController, UIGestureRecognizerDelegate {
     let screen : CGRect  = UIScreen.mainScreen().bounds
     let primitiveEmojiNames = ["🔥","💧","🌎","💨","🌀"]
     let emojisDiscoveredKey = "discovered"
     let emojiCountKey = "count"
     var emojiSize = CGSize()
-    var emojiPaddingFactor = CGFloat(0.1) // space to keep around primitives as fraction of emojiSize.width
-    var primitiveContainerView : PrimitiveContainerView = PrimitiveContainerView(frame: CGRectZero)
-    var counterView = UIBarButtonItem()
-    var discoveredButton = UIButton()
     let comboModel = CombinationModel()
-    var emojisDiscoveredViewController = EmojisDiscoveredViewController()
-    var discoveredNavigationController = UINavigationController()
     var emojisDiscovered = [NSString]()
-    
+    var emojiPaddingFactor = CGFloat(0.1) // space to keep around primitives as fraction of emojiSize.width
+    var movesRemaining: Int = 0
+    var goal : NSString!
+    @IBOutlet weak var movesRemainingView: UILabel!
+    @IBOutlet weak var targetView: UILabel!
     var activeEmojis : [EmojiElement : [NSString]] = Dictionary<EmojiElement, [NSString]>()
+    var primitiveContainerView: PrimitiveContainerView!
     override func viewDidLoad() {
         super.viewDidLoad()
         emojiSize = getEmojiSize()
         addPrimitivesToView()
-        counterView = addCounterToView()
-        if let previouslyDiscoveredEmoji = NSUserDefaults.standardUserDefaults().objectForKey(self.emojisDiscoveredKey) as? Array<NSString> {
-            for emoji in previouslyDiscoveredEmoji {
-                self.emojisDiscovered.append(emoji)
-            }
-            self.counterView.title = makeCounterTitle()
-        }
-    }
-    
-    func addPrimitiveContainer(location: CGPoint, size: CGSize) {
-        let frame = CGRectMake(location.x, location.y, size.width, size.height)
-        let primitiveContainer = PrimitiveContainerView(frame: frame)
-        primitiveContainerView = primitiveContainer
-        self.view.addSubview(primitiveContainer)
-    }
-    
-    func addPrimitivesToView(){
-        let y_location = screen.height - emojiSize.height - (emojiPaddingFactor * 2 * emojiSize.height)
-        let container_y_location = y_location - emojiPaddingFactor * emojiSize.height
-        let container_location = CGPoint(x: 0, y: container_y_location)
-        let container_size = CGSizeMake(screen.width, screen.height-container_y_location)
-        addPrimitiveContainer(container_location, size: container_size)
-        for (i, e) in enumerate(primitiveEmojiNames) {
-            var x_location = emojiPaddingFactor * emojiSize.width + CGFloat(i) * emojiSize.width * (1.0 + emojiPaddingFactor)
-            let newEmoji = createEmojiElement(e)
-            newEmoji.frame.origin = CGPointMake(x_location, y_location)
-            self.view.addSubview(newEmoji)
-        }
-    }
-    
-    func addCounterToView() -> UIBarButtonItem {
-        var counter = UIBarButtonItem()
-        counter.title = makeCounterTitle()
-        let longPressRec = UILongPressGestureRecognizer()
-        longPressRec.addTarget(self, action: "clearDiscovered:")
-        self.view.addGestureRecognizer(longPressRec)
-        counter.style = .Plain
-        counter.target = self
-        counter.action = "displayVC:"
-        self.navigationItem.rightBarButtonItem = counter
-        return counter
-    }
-    
-    // TODO: REMOVE FOR DEPLOYMENT
-    func clearDiscovered(sender: UILongPressGestureRecognizer?){
-        self.emojisDiscovered.removeAll(keepCapacity: false)
-        self.counterView.title = makeCounterTitle()
-    }
-    
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        let controller = segue.destinationViewController as! EmojisDiscoveredViewController
-        controller.discovered = self.emojisDiscovered
-        var emojisDiscovered : [EmojiElement]
-        emojisDiscovered = []
-        for e in controller.discovered {
-            var newElement = createEmojiElement(e)
-            emojisDiscovered.append(newElement)
-        }
-        controller.discoveredEmojis = emojisDiscovered
-    }
-    
-    func displayVC(sender: UIButton!) {
-        println("ATTEMPTING TO DISPLAY VC")
-        performSegueWithIdentifier("displayDiscovered", sender: self)
-    }
-    func dismissVC() {
-        emojisDiscoveredViewController.dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    
-    func getEmojiSize() -> CGSize {
-        let screenWidth = screen.width
-        let screenHeight = screen.height
-        let numPrimitives = CGFloat(primitiveEmojiNames.count)
-        let emojiWidth = screen.width / (numPrimitives * (1+emojiPaddingFactor) + emojiPaddingFactor)
-        let emojiHeight = emojiWidth * 1.2
-        let emojiSize = CGSize(width: emojiWidth, height: emojiHeight)
-        return emojiSize
-    }
-    
-    func createEmojiElement(emojiName: NSString) -> EmojiElement {
-        var textName : NSString? = comboModel.emojiToText[emojiName]
-        if textName == nil {
-            textName = " "
-        }
-        let frame : CGRect = CGRectMake(0, 0, emojiSize.width, emojiSize.height)
-        var elementView = EmojiElement(frame: frame, emojiName : emojiName, textName : textName!)
-        let panRec = UIPanGestureRecognizer()
-        panRec.addTarget(self, action: "draggedEmoji:")
-        let pinchRec = UIPinchGestureRecognizer()
-        pinchRec.addTarget(self, action: "pinchedEmoji:")
-        elementView.addGestureRecognizer(panRec)
-        elementView.addGestureRecognizer(pinchRec)
-        return elementView
+        let level : Level = Level(goal: "🌱", movesToComplete: 5)
+        self.goal = level.goal
+        movesRemaining = level.movesToComplete
+        targetView.text = level.goal as String
+        self.view.addSubview(targetView)
+        movesRemainingView.text = String(level.movesToComplete)
+        self.view.addSubview(movesRemainingView)
+        
     }
     
     func animateCombination(e1 : EmojiElement, e2 : EmojiElement, res : EmojiElement) {
@@ -162,7 +74,7 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         let yAmount = dY/h * movementMagnitude
         let newE1Center = CGPointMake(e1.center.x - xAmount, e1.center.y - yAmount)
         let newE2Center = CGPointMake(e2.center.x + xAmount, e2.center.y + yAmount)
-
+        
         UIView.animateWithDuration(0.3, animations: {
             e1.center = newE1Center
             e2.center = newE2Center
@@ -172,7 +84,7 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     func randomCGFloat() -> CGFloat {
         return CGFloat(Float(arc4random()) / Float(UINT32_MAX))
     }
-
+    
     func draggedEmoji(sender: UIPanGestureRecognizer) {
         var selected : EmojiElement
         if let selected = sender.view as? EmojiElement {
@@ -207,29 +119,44 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
                         selected.center = CGPointMake(selected.center.x, selected.center.y + dy)
                         }, completion: { finished in
                             println("grew") })
-                // remove emojis dragged off the screen
+                    // remove emojis dragged off the screen
                 }
                 else if !CGRectContainsPoint(self.view.frame, selected.center) {
                     println("Attempting to drag off screen")
                     activeEmojis.removeValueForKey(selected)
                     selected.removeFromSuperview()
                 }
-                //    emoji dragged onto canvas
+                    //    emoji dragged onto canvas
                 else {
                     var combined = false
                     let otherEmoji = activeEmojis.keys.array.filter({$0 != selected})
                     for activeEmoji in otherEmoji {
                         if CGRectContainsPoint(activeEmoji.frame, selected.center) {
                             // if the emoji match create the combination
+                            self.movesRemaining -= 1
+                            self.movesRemainingView.text = String(self.movesRemaining)
+                            
                             if let res = combineEmojis(selected, e2: activeEmoji)  {
                                 activeEmojis[res] = [activeEmoji.emojiName, selected.emojiName]
                                 activeEmojis.removeValueForKey(activeEmoji)
                                 activeEmojis.removeValueForKey(selected)
                                 animateCombination(selected, e2: activeEmoji, res: res)
                                 combined = true
+                                println(self.goal)
+                                println(res)
+                                if res.emojiName == self.goal {
+                                    println("You Win!")
+                                    let winLabel = UILabel()
+                                    winLabel.text = "You Win!!"
+                                    winLabel.font = UIFont(name: winLabel.font.fontName, size: 50)
+                                    winLabel.sizeToFit()
+                                    winLabel.textAlignment = .Center
+                                    self.view.addSubview(winLabel)
+                                    winLabel.center = self.view.center
+                                }
                                 break
                             }
-                            // if the emoji don't match display to the user
+                                // if the emoji don't match display to the user
                             else {
                                 println("Emoji didn't match")
                                 animateRejection(activeEmoji, e2: selected)
@@ -241,6 +168,16 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
                             self.activeEmojis[selected] = [NSString()]
                         }
                     }
+                }
+                if self.movesRemaining <= 0 {
+                    println("You lose!")
+                    let loseLabel = UILabel()
+                    loseLabel.text = "You Lose 😭"
+                    loseLabel.font = UIFont(name: loseLabel.font.fontName, size: 50)
+                    loseLabel.sizeToFit()
+                    loseLabel.textAlignment = .Center
+                    self.view.addSubview(loseLabel)
+                    loseLabel.center = self.view.center
                 }
             case .Possible:
                 println("possible")
@@ -295,11 +232,27 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         }
     }
     
-    func makeCounterTitle() -> String {
-        var counterString = String(self.emojisDiscovered.count) + " / " + String(comboModel.accessibleEmoji.count)
-        return counterString
+    func addPrimitiveContainer(location: CGPoint, size: CGSize) {
+        let frame = CGRectMake(location.x, location.y, size.width, size.height)
+        let primitiveContainer = PrimitiveContainerView(frame: frame)
+        primitiveContainerView = primitiveContainer
+        self.view.addSubview(primitiveContainer)
     }
-   
+    
+    func addPrimitivesToView(){
+        let y_location = screen.height - emojiSize.height - (emojiPaddingFactor * 2 * emojiSize.height)
+        let container_y_location = y_location - emojiPaddingFactor * emojiSize.height
+        let container_location = CGPoint(x: 0, y: container_y_location)
+        let container_size = CGSizeMake(screen.width, screen.height-container_y_location)
+        addPrimitiveContainer(container_location, size: container_size)
+        for (i, e) in enumerate(primitiveEmojiNames) {
+            var x_location = emojiPaddingFactor * emojiSize.width + CGFloat(i) * emojiSize.width * (1.0 + emojiPaddingFactor)
+            let newEmoji = createEmojiElement(e)
+            newEmoji.frame.origin = CGPointMake(x_location, y_location)
+            self.view.addSubview(newEmoji)
+        }
+    }
+    
     func combineEmojis(e1: EmojiElement, e2: EmojiElement) -> EmojiElement? {
         let k = (e1.emojiName as String) + (e2.emojiName as String)
         if let newName = comboModel.comboDict[k] {
@@ -310,21 +263,43 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
             }
             // TODO(ih) if saving the whole set is slow, do a more incremental update
             NSUserDefaults.standardUserDefaults().setObject(self.emojisDiscovered, forKey: self.emojisDiscoveredKey)
-//            var counterString = makeCounterTitle()
+            //            var counterString = makeCounterTitle()
             NSUserDefaults.standardUserDefaults().setInteger(self.emojisDiscovered.count, forKey: self.emojiCountKey)
-            self.counterView.title = makeCounterTitle()
             return newEmoji
         }
         return nil
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    func createEmojiElement(emojiName: NSString) -> EmojiElement {
+        var textName : NSString? = comboModel.emojiToText[emojiName]
+        if textName == nil {
+            textName = " "
+        }
+        let frame : CGRect = CGRectMake(0, 0, emojiSize.width, emojiSize.height)
+        var elementView = EmojiElement(frame: frame, emojiName : emojiName, textName : textName!)
+        let panRec = UIPanGestureRecognizer()
+        panRec.addTarget(self, action: "draggedEmoji:")
+        let pinchRec = UIPinchGestureRecognizer()
+        pinchRec.addTarget(self, action: "pinchedEmoji:")
+        elementView.addGestureRecognizer(panRec)
+        elementView.addGestureRecognizer(pinchRec)
+        return elementView
     }
     
+    func getEmojiSize() -> CGSize {
+        let screenWidth = screen.width
+        let screenHeight = screen.height
+        let numPrimitives = CGFloat(primitiveEmojiNames.count)
+        let emojiWidth = screen.width / (numPrimitives * (1+emojiPaddingFactor) + emojiPaddingFactor)
+        let emojiHeight = emojiWidth * 1.2
+        let emojiSize = CGSize(width: emojiWidth, height: emojiHeight)
+        return emojiSize
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
     func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer:UIGestureRecognizer) -> Bool {
         return true
     }
-    
 }
-
